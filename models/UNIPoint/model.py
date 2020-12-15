b2f546958023363da9cc3f337c04fc4e3d7f928f
Kirill Bubenchikov <63867291+bcd8697@users.noreply.github.com>
2020-12-15 13:17:21 +0300

    Update model.py

--- models/UNIPoint/model.py
+++ models/UNIPoint/model.py
@@ -100,7 +100,9 @@ class UNIPoint(nn.Module):
               # calculating numbers of parameters to take for basis function
               par1 = 2 * function
               par2 = 2 * function + 1
-              self.basis_res[:, function] = self.PowerLaw(par1, par2, tau) 
+              if par1 % 3 == 0:
+                
+              self.basis_res[:, function] = self.Exponential(par1, par2, tau) 
               
           self.sum_res = torch.sum(self.basis_res, 1)
           intensity = self.Softplus(self.sum_res) * 0.0000001
